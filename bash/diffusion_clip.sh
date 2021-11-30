4d566dee159b810d70a097c377df98ee6386fbc1
Kao Kitichotkul <rkitichotkul@gmail.com>
2021-11-30 09:51:10 +0000

    hyperparam tuning

--- bash/diffusion_clip.sh
+++ bash/diffusion_clip.sh
@@ -1,12 +1,12 @@
 python diffusion_clip.py \
---text "red car" \
+--text "blue car" \
 --image cifar10-images/4.png \
 --t0 1000 \
---s_inv 1000 \
---s_gen 1000 \
+--s_inv 1500 \
+--s_gen 60 \
 --nudge_iter 1000 \
 --id_weight 1. \
---save_path result/diffclip-sanity-gen3 \
+--save_path result/diffclip3 \
 --config config_yml/cifar10.yml \
 --ckpt model_ckpt/cifar10.ckpt \
 --log_every 50
